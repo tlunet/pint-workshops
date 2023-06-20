2b6953c56badd4ab7e0468d13528c47009fbb319
Thibaut Lunet <thibaut.lunet@tuhh.de>
2023-06-20 11:09:00 +0200

    TL: reformating and s1 for IHP

--- s1/prob2/DahlquistBE.m
+++
@@ -1,8 +0,0 @@
-function [t,y] = DahlquistBE(lam, y0, T, nStep)
-%DAHLQUISTBE  Solve the Dahlquist equation with Backward Euler
-%   TODO
-dt = T/nStep;
-g = 1/(1-lam*dt);
-p = 0:nStep;
-y = g.^p*y0;
-t = linspace(0, T, nStep+1);
